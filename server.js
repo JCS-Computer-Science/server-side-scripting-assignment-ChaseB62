b575cddf913ea97aa8d727da89db28a965580761
Chase Bambrough <28022004@edu.cbe.ab.ca>
2024-10-30 15:27:09 -0600

    AAAAAAAAAAAAAAAAAAAAA

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const uuid = require("uuid");
+const uuid = require("uuid")
 const server = express();
-server.use(express.json());
-server.use(express.static('public'));
+server.use(express.json())
+server.use(express.static('public'))
+
 
 //All your code goes here
 let activeSessions={
@@ -10,47 +11,35 @@ let activeSessions={
 }
 
 server.get('/newgame', (req, res)=>{
-    let newID = uuid.v4();
-    const word = GetWord();
-    let newGame={
-        wordToGuess: word,
-        guesses:[],
-        correctLetters:[],
-        closeLetters:[],
-        rightLetters:[],
-        remainingGuesses: 6,
-        gameOver: false
+let newID = uuid.v4()
+let newGame={
+    wordToGuess: "spike",
+    guesses:[],
+    wrongLetters:[],
+    closeLetters:[],
+    rightLetters: [],
+    remainingGuesses: 6,
+    gameOver: false
+}
+activeSessions[newID] = newGame
+console.log(activeSessions);
 
-    }
-    activeSessions[newID] = newGame
-    console.log(activeSessions);
-
-    res.status(201);
-    res.send({sessionID: newID});
+res.status(201);
+res.send({sessionID: newID});
 
 })
 
 server.get('/gamestate', (req, res) => {
-    let id = req.query.activeSessions;
 
-    res.send(id);
+    res.status(200);
+
+    let gameState = activeSessions[req.query.sessionID]
+    res.send({gameState})
     
 })
+server.get('/newgame', (req, res) => {
 
-// server.get('/guess', (req, res) =>{
-//     console.log("nice guess moron!");
-    
-// })
-
-async function GetWord(){
-    
-    const words = ["clone", "state", "reset", "guess", "local", "blame", "pause", "script", "pulse", "chase", "space", "share", "launch", "words", "serve", "index"]
-    const randomId = Math.floor(Math.random() * words.length);
-    const word = words[randomId];
-
-    return word;
-}
-
+})
 
 //Do not remove this line. This allows the test suite to start
 //multiple instances of your server on different ports
